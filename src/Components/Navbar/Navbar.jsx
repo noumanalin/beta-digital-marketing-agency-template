ddbf75283dfe7b54e5000e4da2a0e44d401a09b8
naumanalin <naumanalin865@gmail.com>
2025-08-12 12:20:49 +0500

    Semi-final version 0.0.3 > Fix navbar import issue

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { FiSearch } from "react-icons/fi";
 import logo from "../../assets/axtra-logo-light-2.webp";
 
 
-export default function Sidebar() { 
+export default function Navbar() { 
   const [menuState, setMenuState] = useState("closed"); // 'closed', 'opening', 'open', 'closing'
   const [showLinks, setShowLinks] = useState(false);
   const openTimeoutRef = useRef(null);
