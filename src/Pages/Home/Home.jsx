b82d94db6565f9c459d7a8667fd767e5f6b11302
naumanalin <naumanalin865@gmail.com>
2025-08-12 15:40:47 +0500

    semi-final version 0.0.7 adding @/components alias

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,22 +1,18 @@
-// IMPORT COMPONENTS HERE
 
-import React from "react";
-import Hero from "../../components/Hero/Hero";
-import Scrollinfinite from "../../components/Scrollinfinite/Scrollinfinite";
-import WorkShowcase from "../../components/Work image gallery/WorkShowcase";
-import Work from "../../components/How we work/work";
-import Brands from "../../components/Brands/Brands";
-import '../../styles/global.css';
-import Testimonials from "../../components/Testimonials";
-import NewsInsight from "../../components/NewsInsight";
-import WorkWithUs from "../../components/WorkWithUs";
-import SolutionSection from "../../components/Services/services";
-import StatsSection from "../../components/Counter/counter";
-import WorkProcess from "../../components/Workflow/workflow";
-
-
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/Footer'
+import Hero from "@/components/Hero/Hero";
+import Scrollinfinite from "@/components/Scrollinfinite/Scrollinfinite";
+import WorkShowcase from "@/components/WorkImageGallery/WorkShowcase";  
+import Work from "@/components/HowWeWork/Work"; 
+import Brands from "@/components/Brands/Brands";
+import "@/styles/global.css";
+import Testimonials from "@/components/Testimonials";
+import NewsInsight from "@/components/NewsInsight";
+import WorkWithUs from "@/components/WorkWithUs";
+import SolutionSection from "@/components/Services/Services";
+import StatsSection from "@/components/Counter/Counter";
+import WorkProcess from "@/components/Workflow/Workflow";
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer";
 
 const Home = () => {
   return (<>
